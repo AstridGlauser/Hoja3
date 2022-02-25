9bb841cdc6f84cbebe17b66c4cc52d6957dcf918
AstridGlauser <84193709+AstridGlauser@users.noreply.github.com>
2022-02-25 14:03:55 -0600

    Update inicio.java

--- inicio.java
+++ inicio.java
@@ -1,5 +1,27 @@
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.BufferedWriter;
 public class inicio {
     public static void main(String args[]) {
+        int cantidad = 3000;
+        String texto = "";
+        for (int i = 0; i < cantidad; i++) {
+            int numero = (int) (Math.random() * cantidad);
+            texto += numero + " ";
+        }
+      
+   
+        try {
+
+            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("numer.txt"));
+			bufferedWriter.write(texto);
+			bufferedWriter.close();
+
+        } catch (IOException exception) {
+         System.out.println("ERROR");
+         
+        } 
 
     }
 }
