4e8c994c4bab7da990dba6200a0ba8980c42cf23
Arthurs101 <84546401+Arthurs101@users.noreply.github.com>
2022-03-04 17:09:14 -0600

    Add files via upload

---
+++ GNOMEIT.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+
+
+import java.util.Collections;
+import java.util.Vector;
+import org.junit.After;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+/**
+ *
+ * @author imarg
+ */
+public class GNOMEIT {
+          @Test
+ 	 public void testEvalute() {
+	
+     GNOME  algoritmo = new GNOME();
+      Vector<Integer> ordenado = new Vector<Integer>();
+      Collections.addAll(ordenado, 1, 3, 31, 42, 55,328,400);
+      Vector<Integer> desordenado = new Vector<Integer>();
+      Collections.addAll(desordenado, 42, 1, 3, 400, 55,31,328);
+      algoritmo.sort(desordenado, 0, 0);
+      assertEquals(ordenado,desordenado);
+	}
+          @Test
+ 	 public void testEvalute2() {
+	
+     GNOME  algoritmo = new GNOME();
+      Vector<Integer> ordenado = new Vector<Integer>();
+      Collections.addAll(ordenado, 1, 2, 3, 4, 5,6,7);
+      Vector<Integer> desordenado = new Vector<Integer>();
+      Collections.addAll(desordenado, 7, 6, 5, 4, 3,2,1);
+      algoritmo.sort(desordenado, 0, 0);
+      assertEquals(ordenado,desordenado);
+	}  @Test
+         public void testEvalute3() {
+	
+     GNOME  algoritmo = new GNOME();
+      Vector<Integer> ordenado = new Vector<Integer>();
+      Collections.addAll(ordenado, 1, 2, 3, 4, 5,6,7);
+      Vector<Integer> desordenado = new Vector<Integer>();
+      algoritmo.sort(desordenado, 0, 0);
+    
+	}
+         
+         	  @Test
+         public void testEvalute4() {
+	
+     GNOME  algoritmo = new GNOME();
+      Vector<Integer> ordenado = new Vector<Integer>();
+      Collections.addAll(ordenado, 1, 2, 3, 4, 5,6,7);
+      Vector<Integer> desordenado = new Vector<Integer>();
+      algoritmo.sort(null, 0, 0);
+    
+	}
+         
+}
