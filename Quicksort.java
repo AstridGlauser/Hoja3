0709508f191ebfcdf4a7d523ef5b8af382eacbcd
AstridGlauser <84193709+AstridGlauser@users.noreply.github.com>
2022-03-04 12:14:50 -0600

    Update Quicksort.java

--- Quicksort.java
+++ Quicksort.java
@@ -3,22 +3,28 @@ import java.util.Vector;
 public class Quicksort<T> implements Comparator<T> {
     @Override
     public void sort(Vector<T> data, int izquierda, int derecha) {
-        // izquierda = 0;
-        // derecha = data.size() - 1;
-        int i, j, pivote;
+        
+        if(data!=null){//Programación defensiva
+         if(data.isEmpty()){//Programación defensiva
+        
+        }else{
+        if(data.get(0).getClass().equals(Integer.class)){//Programación defensiva
+        int i, j, pivote;//inicilización de las variables
+        //elemento menor al pivote a la derecha
+        //elemento mayor al pivote a la izquierda
         i = izquierda;
         j = derecha;
-        pivote = (int) data.get((izquierda + derecha) / 2);
+        pivote = (int) data.get((izquierda + derecha) / 2);//divide el arreglo en 2 para hacer mas sencillo el ordenamiento 
         while (i <= j) {
             while ((int) data.get(i) < (int) pivote) {
-                i++;
+                i++;//incrementa contador i, buscando un numero mayor al  pivote
             }
             while ((int) data.get(j) > (int) pivote) {
-                j--;
+                j--;//disminuye el contador j ,buscando numero menor  pivote
             }
 
-            if (i <= j) {
-                T aux = data.get(i);
+            if (i <= j) {// aqui se realiza el intercambio de posiciones
+                T aux = data.get(i);//corresponde al numero mayor al pivote
                 data.set(i, data.get(j));
                 data.set(j, aux);
 
@@ -28,7 +34,7 @@ public class Quicksort<T> implements Comparator<T> {
             }
         }
 
-        if (izquierda < j) {
+        if (izquierda < j) {//recursivo
             sort(data, izquierda, j);
         }
         if (i < derecha) {
@@ -37,4 +43,7 @@ public class Quicksort<T> implements Comparator<T> {
 
     }
 
+}
+}
+}
 }
